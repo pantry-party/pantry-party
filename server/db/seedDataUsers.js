6b8d41cc3087b6c805acbf695af75858cae69027
ljadow <lindsay@jadow.com>
2024-02-02 15:01:04 -0500

    added 2 more users to houseHold 6

--- server/db/seedDataUsers.js
+++ server/db/seedDataUsers.js
@@ -6,7 +6,9 @@ const users = [
     { id: 3, name: 'Paulina', username: 'paulina3', password: 'paulinapear', color: 'yellow', sharedHouse: null, defaultHouse: 3 },
     { id: 4, name: 'Lindsay', username: 'lindsay4', password: 'lindsaylemon', color: 'green', sharedHouse: null, defaultHouse: 4 },
     { id: 5, name: 'Santa', username: 'sclaus5', password: 'santastrawberry', color: 'blue', sharedHouse: 5, defaultHouse: 7 },
-    { id: 6, name: 'Toothfairy', username: 'tfairy6', password: 'toothtangerine', color: 'purple', sharedHouse: 5, defaultHouse: 8 }
+    { id: 6, name: 'Toothfairy', username: 'tfairy6', password: 'toothtangerine', color: 'purple', sharedHouse: 5, defaultHouse: 8 },
+    { id: 7, name: 'Tinkerbell', username: 'tinkerbell7', password: 'tinkertart', color: 'pink', sharedHouse: 6, defaultHouse: 9 },
+    { id: 8, name: 'Pooh', username: 'pooh8', password: 'poohpumpkin', color: 'violet', sharedHouse: 6, defaultHouse: 10 }
 ]
 
 module.exports = { users }
