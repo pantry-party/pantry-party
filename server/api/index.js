058e3f5191cd6f847db2f68c75bb0dec1d57dc0c
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-02-02 12:11:25 -0800

    server folder index file and api folder index file both on backend

---
+++ server/api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /api/health
+router.get('/health', (req, res, next) => {
+    res.send('OK');
+});
+
+// ROUTER: /api/households
+router.use('/households', require('./households'));
+
+// ROUTER: /api/users
+router.use('/users', require('./users'));
+
+// ROUTER: /api/items
+router.use('/items', require('./items'));
+
+module.exports = router;
