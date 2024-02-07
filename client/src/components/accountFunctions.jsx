b17b156d6d524c33dde4eaed925a19ed9028708b
ljadow <lindsay@jadow.com>
2024-02-07 13:51:11 -0500

    created file structure

---
+++ client/src/components/accountFunctions.jsx
@@ -0,0 +1,1 @@
+//choose/edit color, join household, create household,  add users to household, leave household, log out
---
+++ client/src/components/groceryFunctions.jsx
@@ -0,0 +1,2 @@
+//checkboxes and checking function
+//adding to each category individually
