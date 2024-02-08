a938f164e1314c5ecf04dddcfe5b1a6c8814009f
CaraUhrich <carauhrich@gmail.com>
2024-02-08 16:20:07 -0500

    redux demo code updates

--- client/src/components/PantryList.jsx
+++ client/src/components/PantryList.jsx
@@ -1,6 +1,10 @@
 //content/display of the pantry
+import { useGetPantryItemsbyHouseholdIdQuery } from "../storage/pantryPartyApi"
 
 export default function PantryList () {
 
+    const pantry = useGetPantryItemsbyHouseholdIdQuery(5)
+
+    console.log(pantry)
     return (<div></div>)
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,42 @@
-
+import { useCreateUserMutation } from "../storage/pantryPartyApi"
+import { useState, useEffect } from 'react'
 
 export default function Register () {
+    const [createUser, userCreation] = useCreateUserMutation()
+    console.log(userCreation)
 
-    return (<div></div>)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    if (userCreation.isLoading) {
+        return <div>Loading...</div>
+    }
+
+    async function handleSubmit (e) {
+        e.preventDefault()
+        await createUser({ username, password, defaultHouse: 1, name: 'Test' })
+    }
+
+    return (<div>
+        <form title='Login Form' onSubmit={handleSubmit}>
+                <h3>Sign In</h3>
+                {userCreation.isError && <p>{userCreation.error.error}</p>}
+                <label>Username: 
+                        <input
+                        value={username} 
+                        onChange={(event) => {setUsername(event.target.value)}}
+                        />
+                    </label>
+                    <br/>
+                <label>Password: 
+                    <input
+                        value={password}
+                        type='password'
+                        onChange={(event) => {setPassword(event.target.value)}}
+                    />
+                </label>
+                <br/>
+                <button type="submit">Sign in</button>
+            </form>
+    </div>)
 }
