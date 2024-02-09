ae71d12af743d020a617cd28a7f50062907a6814
CaraUhrich <carauhrich@gmail.com>
2024-02-09 16:44:54 -0500

    quotes to match

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export default function Navigation () {
 
     return (<nav>
-        <Link to={'/'}>Account</Link>
+        <Link to={"/"}>Account</Link>
         <br />
-        <Link to={'/pantry'}>Pantry</Link>
+        <Link to={"/pantry"}>Pantry</Link>
         <br />
-        <Link to={'/groceryList'}>Grocery List</Link>
+        <Link to={"/groceryList"}>Grocery List</Link>
     </nav>)
 }
