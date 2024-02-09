11def87dfe71477162da2d5596fee2eeeef44e92
ljadow <lindsay@jadow.com>
2024-02-09 14:06:40 -0500

    changed seed data to store hashed password for users

--- server/db/seedDataUsers.js
+++ server/db/seedDataUsers.js
@@ -1,14 +1,14 @@
 //id, name, username, password, color, sharedHouse, defaultHouse
 
 const users = [
-    { name: 'Cara', username: 'cara1', password: 'caracarrot', color: 'red', sharedHouse: null, defaultHouse: 1 },
-    { name: 'Sam', username: 'sam2', password: 'samanthastarfruit', color: 'orange', sharedHouse: null, defaultHouse: 2 },
-    { name: 'Paulina', username: 'paulina3', password: 'paulinapear', color: 'yellow', sharedHouse: null, defaultHouse: 3 },
-    { name: 'Lindsay', username: 'lindsay4', password: 'lindsaylemon', color: 'green', sharedHouse: null, defaultHouse: 4 },
-    { name: 'Santa', username: 'sclaus5', password: 'santastrawberry', color: 'blue', sharedHouse: 5, defaultHouse: 7 },
-    { name: 'Toothfairy', username: 'tfairy6', password: 'toothtangerine', color: 'purple', sharedHouse: 5, defaultHouse: 8 },
-    { name: 'Tinkerbell', username: 'tinkerbell7', password: 'tinkertart', color: 'pink', sharedHouse: 6, defaultHouse: 9 },
-    { name: 'Pooh', username: 'pooh8', password: 'poohpumpkin', color: 'violet', sharedHouse: 6, defaultHouse: 10 }
+    { name: 'Cara', username: 'cara1', password: '$2b$10$hO6Vhnq2OAzbknStHEGsw.mv/UMiMxIZ5OWrpBud5PveMS7Uagpce', color: 'red', sharedHouse: null, defaultHouse: 1 },
+    { name: 'Sam', username: 'sam2', password: '$2b$10$uawuW9dLPhxa1dQVLAUYCOBCH5Eob/IQ.jg9z9C8TJVMsTgyeo8xy', color: 'orange', sharedHouse: null, defaultHouse: 2 },
+    { name: 'Paulina', username: 'paulina3', password: '$2b$10$vbO02B87LTaWxATYbbxflut/SzmlqJKU/BlG7ndzFA1A2LQBfHSVi', color: 'yellow', sharedHouse: null, defaultHouse: 3 },
+    { name: 'Lindsay', username: 'lindsay4', password: '$2b$10$8WFgirYUPKblhpaBYNjvTeIugcAmOngwzH5KantUwTOxVMYCaCvJW', color: 'green', sharedHouse: null, defaultHouse: 4 },
+    { name: 'Santa', username: 'sclaus5', password: '$2b$10$taEG58nSF/eoHQbqEMdpQevyzmt9sF7hbNb7k2aS2krybW8qtl32S', color: 'blue', sharedHouse: 5, defaultHouse: 7 },
+    { name: 'Toothfairy', username: 'tfairy6', password: '$2b$10$3yZkNaSva59n80DGP/R1eeT4KPjFfk7bxRrHoaa2sYeRX5ajQmGNa', color: 'purple', sharedHouse: 5, defaultHouse: 8 },
+    { name: 'Tinkerbell', username: 'tinkerbell7', password: '$2b$10$JtAerBdNrLaGQucPRf4Py.rDnPACrfbnXNEyZW45qe7d/UmbiIzyC', color: 'pink', sharedHouse: 6, defaultHouse: 9 },
+    { name: 'Pooh', username: 'pooh8', password: '$2b$10$g8xdwwTw7NpVVANs.E0u.OPe/X7PcP9xVsg4tw0R.Fg79g/cUtBGG', color: 'violet', sharedHouse: 6, defaultHouse: 10 }
 ]
 
 module.exports = { users }
