7bcf787aa13b83de7e4415b09a2322a19936837c
Paulina <paulinasfielek@gmail.com>
2024-02-12 15:04:23 -0800

    applied context at App level to pass more freely

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import React from "react"
+import React, { useContext, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Navigation from "./components/Navigation.jsx"
 import AccountDisplay from "./components/AccountDisplay"
@@ -7,21 +7,27 @@ import PantryList from "./components/PantryList"
 import GroceryList from "./components/GroceryList"
 import GroceryEdit from "./components/GroceryEdit"
 import { bottlesIcon, dairyIcon, dryIcon, freezerIcon, mealsIcon, otherIcon, produceIcon, proteinIcon } from "./styles/icons.jsx"
+import { userContext, householdContext } from "./storage/context.jsx"
 
 
 function App() {
-  
+  const [userInfo, setUserInfo] = useState({})
+  const [household, setHousehold] = useState({})
+
   return (
     <>
-    <Navigation />
+      <Navigation />
       <div>
-        <Routes>
-          <Route path="/" element={<AccountDisplay />} />
-          <Route path="/pantry" element={<PantryList />} />
-          <Route path="/groceryList" element={<GroceryList />} />
-          <Route path="/groceryList/edit" element={<GroceryEdit />} />
-        </Routes>
-
+        <userContext.Provider value={userInfo}>
+          <householdContext.Provider value={household}>
+            <Routes>
+              <Route path="/" element={<AccountDisplay userInfo={userInfo} setUserInfo={setUserInfo} household={household} setHousehold={setHousehold}/>} />
+              <Route path="/pantry" element={<PantryList />} />
+              <Route path="/groceryList" element={<GroceryList />} />
+              <Route path="/groceryList/edit" element={<GroceryEdit />} />
+            </Routes>
+          </householdContext.Provider>
+        </userContext.Provider>
       </div>
     </>
   )
