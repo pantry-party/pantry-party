915098ab5c24a7d97005430368a1f344bf117b6d
Paulina <paulinasfielek@gmail.com>
2024-02-12 12:34:55 -0800

    Added account update forms display

--- client/src/components/AccountDisplay.jsx
+++ client/src/components/AccountDisplay.jsx
@@ -3,71 +3,101 @@ import React from "react"
 import { useContext, createContext, useState } from "react"
 import { userIcon } from "../styles/icons"
 import { useCreateSharedHouseholdMutation, useEditHouseholdMutation, useEditUserMutation, useGetHouseholdbyJoinCodeQuery } from "../storage/pantryPartyApi"
+import Register from "./Register"
+import {colorForm, nameForm, passwordForm, sharedHouseholdForm, joinHouseholdForm, renameHouseholdForm, leaveHouseholdForm, removeMemberForm} from "./AccountFunctions"
 import "../styles/colors.css"
 
-export const userContext = createContext({id: 1, name:"Paulina", color:"pink", sharedHousehold:5})
+export const userContext = createContext({ id: 1, name: "Paulina", color: "pink", sharedHousehold: 5 })
 export const tokenContext = createContext(null)
-export const householdContext = createContext({users: [{id: 2, name: "Cara", color: "yellow"}, {id: 3, name: "Samantha", color: "purple"}, {id: 4, name: "Lindsay", color: "blue"}]})
+export const householdContext = createContext({ users: [{ id: 2, name: "Cara", color: "yellow" }, { id: 3, name: "Samantha", color: "purple" }, { id: 4, name: "Lindsay", color: "blue" }] })
 export const housenameContext = createContext("Paulina's household")
 
 export default function AccountDisplay() {
-    const token = useContext(tokenContext)
+    const [token, setToken] = useState(null)
     const userInfo = useContext(userContext)
     const housename = useContext(housenameContext)
     const household = useContext(householdContext)
     const [createSharedHousehold, sharedHouseholdInfo] = useCreateSharedHouseholdMutation()
     const [editHousehold, editedHousehold] = useEditHouseholdMutation()
     const [editUser, editedUser] = useEditUserMutation()
-    // const [getJoinCode, joinCode] = useGetHouseholdbyJoinCodeQuery()
+    const [displayForm, setDisplayForm] = useState("")
+    console.log(token)
+    const [newColor, setNewColor] = useState("")
+    const [newName, setNewName] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [newHousehold, setNewHousehold] = useState("")
+    const [joinCode, setJoinCode] = useState("")
+    const [newHouseholdName, setNewHouseholdName] = useState("")
+    const [checked, setChecked] = useState(false)
+    const [removal, setRemoval] = useState("") 
 
     const accountInfo = () => {
         return (
-            <div className="accountInfo">
-                <p className={userInfo.color} id={userInfo.id}> {userIcon} Welcome {userInfo.name}! </p>
-                {/* Choose your color */}
-                <button onClick={()=>{editUser(editedUser)}}> Choose your color </button> 
-                {/* Change your name */}
-                <button onClick={()=>{editUser(editedUser)}}> Change your name </button> 
-                {/* Change your password */}
-                <button onClick={()=>{editUser(editedUser)}}> Change your password </button> 
-            </div>
+            <>
+                <div className="accountInfo">
+                    <p className={userInfo.color} id={userInfo.id}> {userIcon} Welcome {userInfo.name}! </p>
+                    {/* Choose your color */}
+                    <button onClick={() => { setDisplayForm("colorForm")}}> Choose your color </button>
+                    {/* Change your name */}
+                    <button onClick={() => { setDisplayForm("nameForm") }}> Change your name </button>
+                    {/* Change your password */}
+                    <button onClick={() => {  setDisplayForm("passwordForm") }}> Change your password </button>
+                    <div className="accountUpdateForms">
+                        {displayForm === "colorForm" && colorForm({newColor, setNewColor})} 
+                        {displayForm === "nameForm" && nameForm({newName, setNewName})} 
+                        {displayForm === "passwordForm" && passwordForm({newPassword, setNewPassword})}
+                        {console.log(newColor)}
+                    </div>
+                </div>
+            </>
         )
     }
 
     const householdInfo = () => {
-        return(
-            <div className = "householdInfo"> 
+        return (
+            <div className="householdInfo">
                 <h3> {housename}</h3>
                 {household.users.map((user) => {
                     return (
                         <p className={user.color} id={user.id}> {userIcon} {user.name} </p>
                     )
                 })}
-            </div> 
+            </div>
         )
     }
 
     const accountOptions = () => {
         return (
-            <div className = "accountOptions">
-                {!userInfo.sharedHousehold && <button onClick={()=>{createSharedHousehold(sharedHouseholdInfo)}}> Create a household to share </button> }
-                {/* <button onClick={()=>{getJoinCode(joinCode)}}> Join a household </button>  */}
-                <button onClick={()=>{editHousehold(editedHousehold)}}> Rename your household </button>
-                {userInfo.sharedHousehold && <button onClick={()=>{editUser(editedUser)}}> Leave this household </button>}
-                {userInfo.sharedHousehold && <button onClick={()=>{editUser(editedUser)}}> Remove a household member </button>}
+            <div className="accountOptions">
+                {!userInfo.sharedHousehold && <button onClick={() => { setDisplayForm("sharedHouseholdForm") }}> Create a household to share </button>}
+                {/* <button onClick={()=>{setDisplayForm("joinHouseholdForm") }}> Join a household </button>  */}
+                <button onClick={() => {  setDisplayForm("renameHouseholdForm")  }}> Rename your household </button>
+                {userInfo.sharedHousehold && <button onClick={() => { setDisplayForm("leaveHouseholdForm")  }}> Leave this household </button>}
+                {userInfo.sharedHousehold && <button onClick={() => {  setDisplayForm("removeMemberForm") }}> Remove a household member </button>}
+                <div className="accountForms"> 
+                    {displayForm === "sharedHouseholdForm" && sharedHouseholdForm({newHousehold, setNewHousehold})} 
+                    {displayForm === "joinHouseholdForm" && joinHouseholdForm({joinCode, setJoinCode})} 
+                    {displayForm === "renameHouseholdForm" && renameHouseholdForm({newHouseholdName, setNewHouseholdName})}
+                    {displayForm === "leaveHouseholdForm" && leaveHouseholdForm({checked, setChecked})} 
+                    {displayForm === "removeMemberForm" && removeMemberForm({removal, setRemoval, household})} 
+                </div> 
             </div>
         )
-        
     }
 
     return (
         <div>
-            {/* {!token && <Login />} */}
-            <div className="accountPage">
-                {accountInfo()}
-                {householdInfo()}
-                {accountOptions()}
-            </div>
+            <userContext.Provider value={userInfo}>
+                {!token && <Register />}
+                {}
+                {
+                    <div className="accountPage">
+                        {accountInfo()}
+                        {householdInfo()}
+                        {accountOptions()}
+                        <button onClick={() => { setToken(null) }}> Log out </button>
+                    </div>}
+            </userContext.Provider>
         </div>
     )
 }
--- client/src/components/AccountFunctions.jsx
+++ client/src/components/AccountFunctions.jsx
@@ -1,6 +1,122 @@
 //choose/edit color, join household, create household,  add users to household, leave household, log out
 
-export default function AccountFunctions () {
+import { useContext } from "react"
 
-    return (<div></div>)
+export default function AccountFunctions() {
+
+    return (
+        <div> </div>
+    )
+}
+
+export const colorForm = ({newColor, setNewColor}) => {
+    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "pink", "gray", "teal", "brown"]
+
+    return (
+        <form id="colorForm" className="accountForm" onSubmit={(e) => { colorFormSubmit(e) }}>
+            <label> Colors:
+                <select required name="colors" onChange={(e) => { setNewColor(e.target.value) }}>
+                    <option value={""}> Select </option>
+                    {colors.map((color) => {
+                        return (
+                            <option value={color} className={color}> {color} </option>
+                        )
+                    })}
+                </select>
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const nameForm = ({newName, setNewName}) => {
+
+    return (
+        <form id="nameForm" className="accountForm" onSubmit={(e) => { nameFormSubmit() }}>
+            <label> New Name:
+                <input type="text" onChange={(e) => { setNewName(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const passwordForm = ({newPassword, setNewPassword}) => {
+
+    return (
+        <form id="passwordForm" className="accountForm" onSubmit={(e) => { passwordFormSubmit(e) }}>
+            <label> New Password:
+                <input type="text" onChange={(e) => { setNewPassword(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const sharedHouseholdForm = ({newHousehold, setNewHousehold}) => {
+
+    return (
+        <form id="sharedHouseholdForm" className="householdForms" onSubmit={(e) => { sharedHouseholdSubmit(e) }}>
+            <label> New Household Name:
+                <input type="text" onChange={(e) => { setNewHousehold(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const joinHouseholdForm = ({joinCode, setJoinCode}) => {
+
+    return (
+        <form id="joinHouseholdForm" className="householdForm" onSubmit={(e) => { joinHouseholdSubmit(e) }}>
+            <label> Join code:
+                <input type="text" onChange={(e) => { setJoinCode(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const renameHouseholdForm = ({newHouseholdName, setNewHouseholdName}) => {
+
+    return (
+        <form id="renameHouseholdForm" className="householdForms" onSubmit={(e) => { renameHouseholdSubmit(e) }}>
+            <label> New Household Name:
+                <input type="text" onChange={(e) => { setNewHouseholdName(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const leaveHouseholdForm = ({checked, setChecked}) => {
+
+    return (
+        <form>
+            <label> Are you sure?
+                <input type="checkbox" onCheck={(e) => { setChecked(e.target.value) }} />
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
+}
+
+export const removeMemberForm = ({removal, setRemoval, household}) => {
+    {console.log(household)}
+    return (
+        <form>
+            <label> Which member would you like to remove?
+                <select name="members" onChange={(e) => { setRemoval(e.target.value) }}>
+                    <option value={""}> Select </option>
+                    {household.users.map((user) => {
+                        return (
+                            <option value={user.id}> {user.name} </option>
+                        )
+                    })}
+
+                </select>
+            </label>
+            <button type="submit" className="submitButton"> Submit </button>
+        </form>
+    )
 }
