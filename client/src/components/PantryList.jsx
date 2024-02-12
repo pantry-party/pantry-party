effc075182f865e8d5d16745841824d3f1a5522c
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-02-12 12:47:57 -0800

    pantry list display where the item names show up on the page

--- client/src/components/PantryList.jsx
+++ client/src/components/PantryList.jsx
@@ -1,10 +1,26 @@
 //content/display of the pantry
 import { useGetPantryItemsbyHouseholdIdQuery } from "../storage/pantryPartyApi"
 
-export default function PantryList () {
+export default function PantryList() {
+  const { data = {}, error, isLoading,} = useGetPantryItemsbyHouseholdIdQuery(5)
 
-    const pantry = useGetPantryItemsbyHouseholdIdQuery(5)
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
 
-    console.log(pantry)
-    return (<div></div>)
+  console.log(data)
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.householdId} >
+          <h2>{item.name}</h2>
+          
+        </div>
+      ))}
+    </div>
+  )
 }
