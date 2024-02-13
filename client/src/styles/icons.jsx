91357e6b333e1e0f281eb0eaedb44f2a91427c62
Paulina <paulinasfielek@gmail.com>
2024-02-12 16:04:53 -0800

    added icons to Account page

--- client/src/styles/icons.jsx
+++ client/src/styles/icons.jsx
@@ -14,16 +14,17 @@ export const produceIcon = <FontAwesomeIcon icon={faAppleWhole} />
 export const proteinIcon = <FontAwesomeIcon icon={faDrumstickBite} />
 
 // alerts icons
-import { faCircleExclamation, faCircleInfo, faPencil, faTrash, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faCircleExclamation, faCircleInfo, faPencil, faTrash, faCirclePlus, faCircle } from '@fortawesome/free-solid-svg-icons'
 
 export const alertIcon = <FontAwesomeIcon icon={faCircleExclamation} />
 export const infoIcon = <FontAwesomeIcon icon={faCircleInfo} />
 export const editIcon = <FontAwesomeIcon icon={faPencil} />
 export const deleteIcon = <FontAwesomeIcon icon={faTrash} />
 export const addIcon = <FontAwesomeIcon icon={faCirclePlus} />
+export const colorIcon = <FontAwesomeIcon icon={faCircle} />
 
 // accounts icons
-import { faCircleUser, faUserPlus, faUserMinus, faHouseUser, faHouseMedical, faHouseCircleXmark, faHouseCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser, faUserPlus, faUserMinus, faHouseUser, faHouseMedical, faHouseCircleXmark, faHouseCircleExclamation, faUserGear, faUserPen } from '@fortawesome/free-solid-svg-icons'
 
 export const userIcon = <FontAwesomeIcon icon={faCircleUser} />
 export const addUsersIcon = <FontAwesomeIcon icon={faUserPlus} />
@@ -32,6 +33,8 @@ export const createHouseholdIcon = <FontAwesomeIcon icon={faHouseUser} />
 export const joinHouseholdIcon = <FontAwesomeIcon icon={faHouseMedical} />
 export const leaveHouseholdIcon = <FontAwesomeIcon icon={faHouseCircleXmark} />
 export const renameHouseholdIcon = <FontAwesomeIcon icon={faHouseCircleExclamation} />
+export const passwordIcon = <FontAwesomeIcon icon={faUserGear} />
+export const nameIcon = <FontAwesomeIcon icon={faUserPen} /> 
 
 // navigation icons
 
