c76469b99d7f7129674c1961690e4b6a0b40c396
CaraUhrich <carauhrich@gmail.com>
2024-02-16 18:05:33 -0500

    items displaying correctly by week

--- client/src/components/PantryList.jsx
+++ client/src/components/PantryList.jsx
@@ -1,89 +1,80 @@
 //content/display of the pantry
-import { useGetPantryItemsbyHouseholdIdQuery } from "../storage/pantryPartyApi";
-import { useContext, useState } from "react";
-import {
-  addIcon,
-  alertIcon,
-  sharingIcon,
-  notSharingIcon,
-} from "../styles/icons";
-import AddItem from "./AddItem";
-import EditItem from "./EditItem";
-import { categoriesContext, userContext } from "../storage/context.jsx";
-import { useSelector } from "react-redux";
+import { useGetPantryItemsbyHouseholdIdQuery } from "../storage/pantryPartyApi"
+import { useContext, useState } from "react"
+import { addIcon, alertIcon, sharingIcon, notSharingIcon } from "../styles/icons"
+import AddItem from "./AddItem"
+import EditItem from "./EditItem"
+import { categoriesContext, userContext } from "../storage/context.jsx"
+import { useSelector } from "react-redux"
 
 export default function PantryList() {
-  const userInfo = useContext(userContext);
-  const householdId = userInfo.sharedHouse || userInfo.defaultHouse;
-  const {
-    data = {},
-    error,
-    isLoading,
-  } = useGetPantryItemsbyHouseholdIdQuery(householdId);
-  const categories = useContext(categoriesContext);
-  const [itemEdit, setItemEdit] = useState(false);
-  const [editId, setEditId] = useState("");
-  const [addForm, setAddForm] = useState(false);
+  const userInfo = useContext(userContext)
+  const householdId = userInfo.sharedHouse || userInfo.defaultHouse
+  const { data = {}, error, isLoading } = useGetPantryItemsbyHouseholdIdQuery(householdId)
+  const categories = useContext(categoriesContext)
+  const [itemEdit, setItemEdit] = useState(false)
+  const [editId, setEditId] = useState("")
+  const [addForm, setAddForm] = useState(false)
 
-  const token = useSelector((it) => it.state.token);
+  const token = useSelector((it) => it.state.token)
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
   if (error) {
-    return <div>Log in to open your pantry...{error.message}</div>;
+    return <div>Log in to open your pantry...{error.message}</div>
   }
 
-  let dateSet = [];
   function createWeeks() {
     // print out all mondays from 1 year ago to today
-    let day;
-    let weekStart;
-    let weekEnd;
-    for (let i = 0; i < 365; i++) {
-      day = new Date();
-      day.setDate(day.getDate() - 365);
-      day.setHours(0, 0, 0, 0);
-      day.setDate(day.getDate() + i);
+    let dateSet = []
+    let day
+    let weekStart
+    let weekEnd
+    for (let i = 0; i < 366; i++) {
+      day = new Date()
+      day.setDate(day.getDate() - 365)
+      day.setHours(0, 0, 0, 0)
+      day.setDate(day.getDate() + i)
       if (day.getDay() === 1) {
-        weekStart = day;
-      } else if (weekStart && day.getDay() === 0) {
-        weekEnd = day;
+        weekStart = day
+      } else if (i === 365 || day.getDay() === 0 && weekStart) {
+        weekEnd = day
       }
 
       if (weekStart && weekEnd) {
-        let week = { weekStart, weekEnd, items: [] };
-        dateSet.push(week);
-        weekStart = null;
-        weekEnd = null;
+        let week = { weekStart, weekEnd, items: [] }
+        dateSet.push(week)
+        weekStart = null
+        weekEnd = null
       }
     }
 
     data.forEach((item) => {
-      let moved = new Date(item.dateMoved);
+      let moved = new Date(item.dateMoved)
+      moved.setHours(0, 0, 0, 0)
       dateSet.forEach((week) => {
-        if (week.weekStart <= moved && moved < week.weekEnd) {
-          console.log("match");
-          week.items.push(item);
+        if (week.weekStart <= moved && moved <= week.weekEnd) {
+          week.items.push(item)
         }
-      });
-    });
+      })
+    })
 
-    console.log(dateSet);
-
-    return dates;
+    return dateSet
   }
-  createWeeks();
+
+  const weeksArr = createWeeks()
+  console.log(weeksArr[weeksArr.length - 1])
 
   //edit items
   function itemEditor(itemId) {
     if (itemEdit == false) {
-      setItemEdit(!itemEdit);
-      setEditId(itemId);
+      setItemEdit(!itemEdit)
+      setEditId(itemId)
     } else if (itemEdit == true && itemId == editId) {
-      setItemEdit(!itemEdit);
+      setItemEdit(!itemEdit)
     } else if (itemEdit == true) {
-      setEditId(itemId);
+      setEditId(itemId)
     }
   }
 
@@ -96,9 +87,9 @@ export default function PantryList() {
   };
 
   function parseDate(timestamp) {
-    const date = new Date(timestamp);
+    const date = new Date(timestamp)
 
-    return date.toLocaleDateString(undefined, dateType);
+    return date.toLocaleDateString(undefined, dateType)
   }
 
   return (
@@ -109,7 +100,7 @@ export default function PantryList() {
         <button
           title="Add New Item"
           onClick={() => {
-            setAddForm(!addForm);
+            setAddForm(!addForm)
           }}
         >
           {addIcon}
@@ -123,43 +114,36 @@ export default function PantryList() {
 
       {/* display items and icons */}
       <div>
-        {data.map((item) => (
-          <div key={item.id} className={item.category}>
-            {/* edit item button */}
-            <span className={item.color}>
-              <button
-                title="Edit Item Details"
-                onClick={() => {
-                  itemEditor(item.id);
-                }}
-              >
-                {
-                  categories.find(
-                    (category) => item.category === category.name.toLowerCase()
-                  ).icon
-                }
-              </button>
-            </span>
-
-            {console.log(item)}
-
-            <h2>
-              {item.name}{" "}
-              {item.isLow && <span className="blue"> {alertIcon}</span>}{" "}
-            </h2>
-            <p>{parseDate(item.dateMoved)}</p>
-
-            {/* display alerts */}
-            {item.sharing && <div className="green"> {sharingIcon}</div>}
-            {item.sharing === false && (
-              <div className="red"> {notSharingIcon}</div>
-            )}
-
-            {itemEdit && item.id === editId && (
-              <EditItem item={item} user={userInfo} />
-            )}
-          </div>
-        ))}
+        {weeksArr.map((week) => {
+          if (week.items.length) {
+            return (<div key={week.weekStart}>
+              <h2>Week of {week.weekStart.toLocaleDateString()} - {week.weekEnd.toLocaleDateString()}</h2>
+              {week.items.map((item) => (
+                <div key={item.id} className={item.category}>
+                  {/* edit item button */}
+                  <span className={item.color}>
+                    <button
+                      title="Edit Item Details"
+                      onClick={() => {
+                        itemEditor(item.id)
+                      }}
+                    >
+                      {categories.find((category) => item.category === category.name.toLowerCase()).icon}
+                    </button>
+                  </span>
+                  <h2>
+                    {item.name} {item.isLow && <span className="blue"> {alertIcon}</span>}{" "}
+                  </h2>
+                  <p>{parseDate(item.dateMoved)}</p>
+                  {/* display alerts */}
+                  {item.sharing && <div className="green"> {sharingIcon}</div>}
+                  {item.sharing === false && <div className="red"> {notSharingIcon}</div>}
+                  {itemEdit && item.id === editId && <EditItem item={item} user={userInfo} />}
+                </div>
+              ))}
+            </div>)
+          }
+        })}
       </div>
     </>
   );
