0bc95820573820025bf834d86345e2cc413912f5
Paulina <paulinasfielek@gmail.com>
2024-02-20 17:34:59 -0500

    added nav for desktop

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,20 +2,24 @@
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import KeyAccordion from './KeyAccordion'
-import { plusIcon, minusIcon, downIcon, upIcon } from "../styles/icons"
+import { plusIcon, pantryIcon, downIcon, upIcon } from "../styles/icons"
+import  "../styles/nav.css"
 
 export default function Navigation() {
     const [showKey, setShowKey] = useState(false)
 
     return (<nav>
-        <Link to={"/"}>Account</Link>
-        <br />
-        <Link to={"/pantry"}>Pantry</Link>
-        <br />
-        <Link to={"/groceryList"}>Grocery List</Link>
-        <br />
+        <div className="party">
+           <p className="navIcon"> {pantryIcon} </p> 
+        </div>
+        <div className="navItems">
+        <Link to={"/"} className="navLink">Account</Link>
+        <Link to={"/pantry"} className="navLink">Pantry</Link>
+        <Link to={"/groceryList"} className="navLink">Grocery List</Link>
+        </div> 
+        <div className="key">
         <Link onClick={() => { setShowKey(!showKey) }}>Key {!showKey ? downIcon : upIcon}</Link>
-        <br />
         {showKey && <KeyAccordion />}
+        </div>
     </nav>)
 }
