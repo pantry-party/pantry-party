8f28b80089885d0d9e6057ac12a49ff33497f7db
ljadow <lindsay@jadow.com>
2024-02-21 14:13:59 -0500

    creating a deploy branch

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,9 @@ const { Client } = require('pg')
 
 //Establishing the connection to the database (like how we do with http://)
 const { DATABASE_URL } = require('../secrets') 
-// const connectionString = process.env.DATABASE_URL
+
+//Render database URL -> to replace in secrets
+//"postgres://pantrypartydb_user:COvnzEu7fmAz7rrd7AS6gsGhsk5an2IG@dpg-cnb4hcun7f5s73emm6tg-a/pantrypartydb"
 
 const client = new Client(DATABASE_URL)
 
