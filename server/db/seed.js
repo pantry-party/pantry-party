e3133aeb1205221565c9879b95ad00145141196d
CaraUhrich <carauhrich@gmail.com>
2024-02-22 16:44:22 -0500

    moved the joinCode fn into javascript

--- server/db/seed.js
+++ server/db/seed.js
@@ -5,21 +5,6 @@ const { households } = require('./seedDataHouseholds')
 const { users } = require('./seedDataUsers')
 const { items } = require('./seedDataItems')
 
-//Drop functions for data cleanliness
-const dropFunctions = async () => {
-    try {
-        console.log("Starting to drop functions...")
-        await client.query(`
-        DROP FUNCTION IF EXISTS gen_random_bytes;
-        DROP FUNCTION IF EXISTS random_string;
-        DROP FUNCTION IF EXISTS unique_random;
-        `)
-        console.log("Functions dropped!")
-    } catch (error) {
-        console.log("Error dropping functions: ", error)
-    }
-}
-
 //Drop tables for data cleanliness
 const dropTables = async () => {
     try {
@@ -133,56 +118,6 @@ const createInitialItems = async () => {
     }
 }
 
-//join code generation functions by Derek Sivers. Article: https://sive.rs/rand1
-async function joinCodeFns() {
-    try {
-        console.log('Creating functions...')
-        await client.query(`
-            create function gen_random_bytes(int) returns bytea as
-            '$libdir/pgcrypto', 'pg_random_bytes' language c strict;
-        `)
-        await client.query(`
-            create function random_string(len int) returns text as $$
-            declare
-            chars text[] = '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
-            result text = '';
-            i int = 0;
-            rand bytea;
-            begin
-            -- generate secure random bytes and convert them to a string of chars.
-            rand = gen_random_bytes($1);
-            for i in 0..len-1 loop
-                -- rand indexing is zero-based, chars is 1-based.
-                result = result || chars[1 + (get_byte(rand, i) % array_length(chars, 1))];
-            end loop;
-            return result;
-            end;
-            $$ language plpgsql;
-        `)
-        await client.query(`
-            create function unique_random(len int, _table text, _col text) returns text as $$
-            declare
-            result text;
-            numrows int;
-            begin
-            result = random_string(len);
-            loop
-                execute format('select 1 from %I where %I = %L', _table, _col, result);
-                get diagnostics numrows = row_count;
-                if numrows = 0 then
-                return result; 
-                end if;
-                result = random_string(len);
-            end loop;
-            end;
-            $$ language plpgsql;
-        `)
-        console.log('Functions created!')
-    } catch (error) {
-        throw error
-    }
-}
-
 //Call all our functions to build our database
 const buildDb = async () => {
     try {
@@ -190,10 +125,8 @@ const buildDb = async () => {
         client.connect()
 
         //Run our functions
-        await dropFunctions()
         await dropTables()
         await createTables()
-        await joinCodeFns()
         await createInitialHouseholds()
         await createInitialUsers()
         await createInitialItems()
