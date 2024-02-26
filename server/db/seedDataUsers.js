7cdce1f0e5ec27f7d177d6789383317985bc45b2
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-02-26 13:18:10 -0800

    code cleanup

--- server/db/seedDataUsers.js
+++ server/db/seedDataUsers.js
@@ -5,8 +5,6 @@ const users = [
     { name: 'Sam', username: 'sam2', password: '$2b$10$uawuW9dLPhxa1dQVLAUYCOBCH5Eob/IQ.jg9z9C8TJVMsTgyeo8xy', color: 'orange', sharedHouse: null, defaultHouse: 2 },
     { name: 'Paulina', username: 'paulina3', password: '$2b$10$vbO02B87LTaWxATYbbxflut/SzmlqJKU/BlG7ndzFA1A2LQBfHSVi', color: 'yellow', sharedHouse: null, defaultHouse: 3 },
     { name: 'Lindsay', username: 'lindsay4', password: '$2b$10$8WFgirYUPKblhpaBYNjvTeIugcAmOngwzH5KantUwTOxVMYCaCvJW', color: 'green', sharedHouse: null, defaultHouse: 4 },
-
-
     { name: 'Santa', username: 'sclaus5', password: '$2b$10$taEG58nSF/eoHQbqEMdpQevyzmt9sF7hbNb7k2aS2krybW8qtl32S', color: 'red', sharedHouse: 5, defaultHouse: 7 },
     { name: 'Toothfairy', username: 'tfairy6', password: '$2b$10$3yZkNaSva59n80DGP/R1eeT4KPjFfk7bxRrHoaa2sYeRX5ajQmGNa', color: 'purple', sharedHouse: 5, defaultHouse: 8 },
     { name: 'Tinkerbell', username: 'tinkerbell7', password: '$2b$10$JtAerBdNrLaGQucPRf4Py.rDnPACrfbnXNEyZW45qe7d/UmbiIzyC', color: 'pink', sharedHouse: 5, defaultHouse: 9 },
