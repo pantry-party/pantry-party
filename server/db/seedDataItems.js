8548ead6f56cb7611c91dab074acb6937c649a5a
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-02-26 12:34:58 -0800

    final data seed cleanup and edits for demo santa household account

--- server/db/seedDataItems.js
+++ server/db/seedDataItems.js
@@ -6,19 +6,19 @@ const items = [
         sharing: null,
         isLow: false,
         category: "produce",
-        expiry: null, 
-        ownerId: null,
+        expiry: "2024-01-08", 
+        ownerId: 7,
         householdId: 5
     },
     {
-        name: "brocolli",
+        name: "broccoli",
         dateMoved: "2024-01-01",
         inPantry: true, 
         sharing: null,
         isLow: false,
         category: "produce",
-        expiry: null, 
-        ownerId: null,
+        expiry: "2024-01-08", 
+        ownerId: 8,
         householdId: 5
     },
     {
@@ -28,7 +28,7 @@ const items = [
         sharing: null,
         isLow: true,
         category: "dairy",
-        expiry: null, 
+        expiry: "2024-01-06", 
         ownerId: null,
         householdId: 5
     },
@@ -61,7 +61,7 @@ const items = [
         sharing: true,
         isLow: false,
         category: "cans & bottles",
-        expiry: null, 
+        expiry: "2024-01-22", 
         ownerId: 5,
         householdId: 5
     },
@@ -72,6 +72,17 @@ const items = [
         sharing: null,
         isLow: false,
         category: "cans & bottles",
+        expiry: null, 
+        ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "parmesan cheese",
+        dateMoved: "2024-01-15",
+        inPantry: true, 
+        sharing: null,
+        isLow: false,
+        category: "dairy",
         expiry: null, 
         ownerId: null,
         householdId: 5
@@ -89,7 +100,7 @@ const items = [
     },
     {
         name: "fusili",
-        dateMoved: "2024-01-15",
+        dateMoved: "2024-01-22",
         inPantry: true, 
         sharing: null,
         isLow: false,
@@ -128,7 +139,128 @@ const items = [
         isLow: false,
         category: "other",
         expiry: null, 
+        ownerId: 8,
+        householdId: 5
+    },
+    {
+        name: "spaghetti",
+        dateMoved: "2024-01-22",
+        inPantry: true, 
+        sharing: null,
+        isLow: false,
+        category: "dry goods",
+        expiry: null, 
         ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "lettuce",
+        dateMoved: "2024-01-29",
+        inPantry: true, 
+        sharing: null,
+        isLow: false,
+        category: "produce",
+        expiry: "2024-02-05", 
+        ownerId: 7,
+        householdId: 5
+    },
+    {
+        name: "cheese",
+        dateMoved: "2024-01-29",
+        inPantry: false, 
+        sharing: null,
+        isLow: false,
+        category: "dairy",
+        expiry: null, 
+        ownerId: 8,
+        householdId: 5
+    },
+    {
+        name: "chocolate",
+        dateMoved: "2024-02-05",
+        inPantry: true, 
+        sharing: null,
+        isLow: true,
+        category: "other",
+        expiry: null, 
+        ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "strawberries",
+        dateMoved: "2024-02-05",
+        inPantry: true, 
+        sharing: null,
+        isLow: false,
+        category: "produce",
+        expiry: "2024-02-07", 
+        ownerId: 6,
+        householdId: 5
+    },
+    {
+        name: "chicken",
+        dateMoved: "2024-02-05",
+        inPantry: true, 
+        sharing: null,
+        isLow: false,
+        category: "proteins",
+        expiry: "2024-02-07", 
+        ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "apple juice",
+        dateMoved: "2024-02-05",
+        inPantry: true, 
+        sharing: false,
+        isLow: false,
+        category: "cans & bottles",
+        expiry: null, 
+        ownerId: 7,
+        householdId: 5
+    },
+    {
+        name: "bananas",
+        dateMoved: "2024-02-12",
+        inPantry: false, 
+        sharing: false,
+        isLow: false,
+        category: "produce",
+        expiry: null, 
+        ownerId: 8,
+        householdId: 5
+    },
+    {
+        name: "popsicles",
+        dateMoved: "2024-02-12",
+        inPantry: false, 
+        sharing: false,
+        isLow: false,
+        category: "freezer",
+        expiry: null, 
+        ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "orange juice",
+        dateMoved: "2024-02-12",
+        inPantry: false, 
+        sharing: false,
+        isLow: false,
+        category: "cans & bottles",
+        expiry: null, 
+        ownerId: null,
+        householdId: 5
+    },
+    {
+        name: "rice",
+        dateMoved: "2024-02-19",
+        inPantry: false, 
+        sharing: false,
+        isLow: false,
+        category: "dry goods",
+        expiry: null, 
+        ownerId: 5,
         householdId: 5
     }
 ]
